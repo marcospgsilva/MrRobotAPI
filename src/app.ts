4fcaf156f37bc8a4b7aa2e8580f626645188caaa
Marcos Paulo <marcospg-silva@hotmail.com>
2020-12-17 16:05:21 -0300

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import bodyParser from 'body-parser'
 
 import routes from './routes'
@@ -6,6 +7,7 @@ import routes from './routes'
 const app = express()
 const port = process.env.PORT || 3333
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(routes)
 app.listen(port)
